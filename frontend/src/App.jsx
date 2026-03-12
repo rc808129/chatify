6c3677c48438f48cb85142f1ae9cb0a5ff818dd8
rc808129 <rc808129@gmail.com>
2026-03-12 19:40:48 +0530

    ready for deployment frontend add env file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function App() {
   const [count, setCount] = useState(0)
 
