e2ea23db123dce04ba0e4c759bf4cbff4eda6814
rc808129 <rc808129@gmail.com>
2026-03-13 16:15:07 +0530

    signup branch created

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+
+
+export const generateToken = (useId, res) => {
+  const { JWT_SECRET} = process.env;
+  if(!JWT_SECRET){
+    throw new Error("JWT_SECRET is not configured")
+  }
+  const token = jwt.sign({useId}, JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 *24 *60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  return token
+
+}
